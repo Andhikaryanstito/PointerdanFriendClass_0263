b8c8b38791b76c6161cdcb42f73fd75a1afeee1a
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2025-05-06 11:40:19 +0700

    membuat definisi pelajar dengan akses friend

--- friendfunctiondua.cpp
+++ friendfunctiondua.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 using namespace std;
 
+class pelajar;  
+class manusia {
+public:
+    void showNilaiPelajar(pelajar &x);
+};
+class pelajar {
+    int nilai;
+
+public:
+    pelajar() { nilai = 100; }
+
+    // Memberikan akses ke manusia::showNilaiPelajar
+    friend void manusia::showNilaiPelajar(pelajar &x);
+};
