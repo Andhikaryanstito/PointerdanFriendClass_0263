90f252dff4dbc5ee272f47e664b5671cc97e8893
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2025-05-06 11:25:46 +0700

    membuat  kelas dan deklarasi mahasiswa

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa{
+class Mahasiswa{
     public:
     int nim;
     void showNim(){
         cout <<"No induk =" <<nim<<endl;
     }
 };
+int main (){
+
+    Mahasiswa mhs{1}; // objek mhs
+    mhs.showNim(); // member access operator
+
+    Mahasiswa &refMhs = mhs; //pointer reference refmhs
+    refMhs.nim = 2; // member access operator
+    mhs.showNim();
+
+    Mahasiswa *pMhs = &mhs; //pointer dereference pmhs
+    pMhs->nim = 3 ;//arrow operator
+    pMhs->showNim();
+    return 0;
+}
